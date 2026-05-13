a1e765cca49dfe5cf12ce616b0fc77b7c066057e
hutagalung12 <email@yudaroytoba@gmail.com>
2026-05-13 17:53:57 +0700

    membuat models kendaraan

--- app/Models/Kendaraan.php
+++ app/Models/Kendaraan.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kendaraan extends Model
 {
-    //
+    /**
+     * Nama tabel yang terkait dengan model.
+     * Secara default Laravel akan menganggap tabelnya bernama 'kendaraans'.
+     */
+    protected $table = 'kendaraans';
+
+    /**
+     * Atribut yang dapat diisi melalui Mass Assignment.
+     * Sesuai instruksi poin nomor 2 pada soal.
+     */
+    protected $fillable = [
+        'plat_nomor',
+        'nama_pemilik',
+        'merk_kendaraan',
+        'keluhan',
+    ];
 }
